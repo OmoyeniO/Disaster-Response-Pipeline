a0f0735c12d6efee3bb6be9a0d11bc9f9868fdbb
Omoyeni Ogundipe <90872230+OmoyeniO@users.noreply.github.com>
2022-09-27 05:59:12 -0500

    finished building models in jupyter notebook

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,16 +1,70 @@
 import sys
-
+import sys
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.base import BaseEstimator, TransformerMixin
+import joblib 
+import lightgbm as lgb
+from sklearn.decomposition import TruncatedSVD
+import pickle
 
 def load_data(database_filepath):
-    pass
+    '''
+     Load data from database file path
+     Output feature set, target and target categories
+    
+     database_filepath: database file path
+    '''
+    # load data from database
+    engine = create_engine('sqlite:///Disaster_response.db') 
+    df = pd.read_sql_table('Messages', engine)
+    X = df['message']
+    Y = df.drop(columns=['id','message','original','genre'])   
+
+    return X, Y, Y.columns
 
 
 def tokenize(text):
-    pass
+    '''
+    Tokenize, lemmatize, normalize, strip, remove stop words from the text
+    
+    :param text: input text
+    '''
+    # Initialization
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    stopWords = set(stopwords.words('english'))
+    
+    # Get clean tokens after lemmatization, normalization, stripping and stop words removal
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        if tok not in stopWords:
+            clean_tokens.append(clean_tok)
+
+    return clean_tokens
+    
 
 
 def build_model():
-    pass
+    '''
+    Build a pipeline with 
+    '''
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
